7427e06c430bed7c96edf4c73ad434cff9e10705
Ian Jiang <ianjiang.ict@gmail.com>
2020-01-10 10:45:38 +0800

    riscv: Add new machine multico based spike
    The new machine type multico works as a multi-core RISCV system with only
    - A samll piece of ROM starting at 0x1000 of size 0x1000.
    - A large RAM start at 0x2000
    and without any other peripheral.
    
    To keep the entry address of guest ELF _start to be 0x80000000, and the
    memory size must be large enough to contain that space. This means:
    
        RAM_start + RAM_size > ELF_start
        RAM_size > ELF_start - RAM_start = 0x80000000 - 0x2000 = 2,147,475,456
    
    Example running (memory_size = 4G):
        qemu-system-riscv64 -nographic \
                -machine multico       \
                -m 4G                  \
                -kernel ../riscv-probe/build/bin/rv64imac/spike/hello
    
    Signed-off-by: Ian Jiang <ianjiang.ict@gmail.com>

---
+++ include/hw/riscv/multico.h
@@ -0,0 +1,31 @@
+#ifndef HW_RISCV_MULTICO_H
+#define HW_RISCV_MULTICO_H
+
+#include "hw/riscv/riscv_hart.h"
+#include "hw/sysbus.h"
+
+typedef struct {
+    /*< private >*/
+    SysBusDevice parent_obj;
+
+    /*< public >*/
+    RISCVHartArrayState soc;
+    void *fdt;
+    int fdt_size;
+} MulticoState;
+
+enum {
+    MULTICO_MROM,
+    MULTICO_CLINT,
+    MULTICO_DRAM
+};
+
+#define MULTICO_START 0x80000000
+
+#if defined(TARGET_RISCV32)
+#define MULTICO_CPU TYPE_RISCV_CPU_RV32GCSU_V1_10_0
+#elif defined(TARGET_RISCV64)
+#define MULTICO_CPU TYPE_RISCV_CPU_RV64GCSU_V1_10_0
+#endif
+
+#endif
